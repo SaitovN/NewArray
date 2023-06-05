ef05684cc42a43893919f5d998458017ee324da9
SaitovN <nuriksaitov03@gmailcom>
2023-06-05 16:37:06 +0600

    Create NewArray.java
    NewArray

---
+++ NewArray.java
@@ -0,0 +1,22 @@
+package academy.devonline.java.basic.section01_setup.section06_setup;
+
+public class NewArray {
+    public static void main(String[] args) {
+        int[]arrayint = {1,2,3,4,5};
+        double[] arrayduuble ={1.,2.,3.,4.,5.};
+        boolean[]arrayboolean = {true,false,true,false};
+        char[]arraychar = {'1','2','3','4','5'};
+        String[]arraystring = {"1","2","3","4","5"};
+        {
+            int [] arrayin = new int[5];
+            double [] arraydub = new double[5];
+            boolean [] arraybool = new boolean[5];
+            char [] arrachar = new char[5];
+            String [] arraytring = new String[5];
+        }
+        {
+            int[] emptyArray1 = {};
+            int[] emptyArray2 = new int[0];
+        }
+    }
+}
